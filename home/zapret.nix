7eb5315b9ba8156ebd3d7d4d1bd4915afe29f520
Odrian <adrian46tver@bk.ru>
2025-06-08 23:17:47 +0300

    simplify zapret.nix

--- home/zapret.nix
+++ home/zapret.nix
@@ -1,54 +1,59 @@
 { config, pkgs, ... }:
 
-let
-  settings = import ../settings.nix;
+# let
+#   settings = import ../settings.nix;
 
-  zapret-src = pkgs.fetchFromGitHub {
-    owner = "Sergeydigl3";
-    repo = "zapret-discord-youtube-linux";
-    rev = "59301aaba4f97a24fa416007cb06e00ab6e897a9";
-    sha256 = "sha256-MxHBjCw+gQ4IWaJI3KYXmeLFmKNu9PHEtSz8nIK8gPA=";
-  };
+#   zapret-src = pkgs.fetchFromGitHub {
+#     owner = "Sergeydigl3";
+#     repo = "zapret-discord-youtube-linux";
+#     rev = "59301aaba4f97a24fa416007cb06e00ab6e897a9";
+#     sha256 = "sha256-MxHBjCw+gQ4IWaJI3KYXmeLFmKNu9PHEtSz8nIK8gPA=";
+#   };
 
-  configText = ''
-    interface=${settings.zapret.wifi}
-    auto_update=false
-    strategy=${settings.zapret.strategy}
-  '';
+#   configText = ''
+#     interface=${settings.zapret.wifi}
+#     auto_update=false
+#     strategy=${settings.zapret.strategy}
+#   '';
 
-  repoPath = "$HOME/.zapret";
-  confEnvPath = "${repoPath}/conf.env";
-  scriptPath = "${repoPath}/main_script.sh";
-in {
-  home.packages = [ pkgs.bash ];
+#   repoPath = "$HOME/.zapret";
+#   confEnvPath = "${repoPath}/conf.env";
+#   scriptPath = "${repoPath}/main_script.sh";
+# in
+{
+  home.packages = with pkgs; [ bash nftables ];
 
   # update config
-  home.activation.zapretConf = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    rm -rf "${repoPath}"
-    mkdir -p "${repoPath}"
-    cp -r ${zapret-src}/* "${repoPath}"
-    chmod -R u+w "${repoPath}"
-    echo "${configText}" > ${confEnvPath}
-  '';
+  # home.activation.zapretConf = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+  #   rm -rf "${repoPath}"
+  #   mkdir -p "${repoPath}"
+  #   cp -r ${zapret-src}/* "${repoPath}"
+  #   chmod -R u+w "${repoPath}"
+  #   echo "${configText}" > ${confEnvPath}
+  # '';
 
   home.file."zapret.sh".text = ''
-    bash ${scriptPath} -nointeractive
+GIT_LINK="https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git"
+TARGET_DIR="$HOME/.zapret"
+BASH_SCRIPT="$TARGET_DIR/main_script.sh"
+
+HARD_MODE=false
+if [[ "$1" == "--hard" ]]; then
+  HARD_MODE=true
+fi
+
+# Клонирование репозитория
+if [[ -d "$TARGET_DIR" ]]; then
+  if $HARD_MODE; then
+    echo "Клонирование репозитория в $TARGET_DIR..."
+    rm -rf "$TARGET_DIR"
+    git clone "$GIT_LINK" "$TARGET_DIR"
+  fi
+else
+  echo "Клонирование репозитория в $TARGET_DIR..."
+  git clone "$GIT_LINK" "$TARGET_DIR"
+fi
+
+bash $BASH_SCRIPT
   '';
-
-  # systemd user unit
-  systemd.user.services.zapret = {
-    Unit = {
-      Description = "Run zapret-discord-youtube-linux";
-      After = [ "default.target" ];
-    };
-
-    Service = {
-      ExecStart = "sudo ${pkgs.bash}/bin/bash ~/zapret.sh";
-      Restart = "on-failure";
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
