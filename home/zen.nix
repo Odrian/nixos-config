f4da0a08a4aa53047822a4c479e826af588894fe
Odrian <radukaa@mail.ru>
2025-08-19 21:40:38 +0300

    update zen.nix flake, remove zen from autostart

--- home/zen.nix
+++ home/zen.nix
@@ -2,7 +2,7 @@
 
 let
   # Импорт flake вручную
-  zen-browser-flake = builtins.getFlake "github:Gurjaka/zen-browser-nix";
+  zen-browser-flake = builtins.getFlake "github:0xc000022070/zen-browser-flake";
   zen-browser = zen-browser-flake.packages.${pkgs.system}.default;
 in
 {
@@ -10,10 +10,10 @@ in
     zen-browser
   ];
 
-  home.file.".config/autostart/zen.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=Zen browser
-    Exec=zen
-  '';
+  # home.file.".config/autostart/zen.desktop".text = ''
+  #   [Desktop Entry]
+  #   Type=Application
+  #   Name=Zen browser
+  #   Exec=zen
+  # '';
 }
