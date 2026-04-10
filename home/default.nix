da90bd37bde2db241fc970081915a28d82741861
Odrian <radukaa@mail.ru>
2026-04-10 18:31:10 +0300

    add bash prefix

--- home/default.nix
+++ home/default.nix
@@ -20,7 +20,15 @@ in
 
     ./rust.nix
     ./cpp.nix
-
-    ./dedub.nix
   ];
+
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      # bash prefix
+      export PS1='\[\e[32m\]┌──(\[\e[94;1m\]\u\[\e[94m\]@\[\e[94m\]\h\[\e[0;32m\])-[\[\e[38;5;46;1m\]\w\[\e[0;32m\]] [\[\e[32m\]$?\[\e[32m\]]\n\[\e[32m\]╰─\[\e[94;1m\]\$\[\e[0m\] '
+      # highlight in ls .nix with purple
+      export LS_COLORS="$LS_COLORS:*.nix=01;35"
+    '';
+  };
 }
