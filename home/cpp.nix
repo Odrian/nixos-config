1db4fed0f7535d85c3a0363d49a01457b55ac0df
Odrian <radukaa@mail.ru>
2026-04-10 18:06:28 +0300

    fix cpp.nix, rust.nix

--- home/cpp.nix
+++ home/cpp.nix
@@ -7,6 +7,10 @@
     clang-analyzer
     gnumake cmake
     ccache
+    ninja
+    pkg-config
+    perf
+    valgrind
 
     (jetbrains.clion.override { forceWayland = true; })
   ];
--- home/rust.nix
+++ home/rust.nix
@@ -5,11 +5,11 @@
     rustup
     sccache
 
-    (jetbrains.rust-rover.override { forceWayland = true; })
+    jetbrains.rust-rover
   ];
 
   home.sessionVariables = {
     RUSTC_WRAPPER = "sccache";
-    SCCACHE_CACHE_SIZE = "10G"; # можно указать размер кэша
+    SCCACHE_CACHE_SIZE = "10G";
   };
 }
